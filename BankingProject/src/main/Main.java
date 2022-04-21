ba62565d9bd041d5e3ffd30ca531abfae59eb7d1
unknown <ptownderek@yahoo.com>
2022-04-21 09:21:58 -0700

    adding generics, testing in main

--- BankingProject/src/main/Main.java
+++ BankingProject/src/main/Main.java
@@ -22,6 +22,27 @@ public class Main {
 		System.out.println(empTwo);
 		System.out.println(empThree);
 		System.out.println(mang1);
+		
+		
+		Box<Integer> intBox = new Box<Integer>();
+		intBox.add(333);
+		intBox.add(343);
+		intBox.add(323);
+		intBox.add(313);
+		intBox.add(33);
+		
+		System.out.println(intBox.get());
+		
+		Box<String> strBox = new Box<String>();
+		
+				strBox.add("duck");
+				strBox.add("chicken");
+				strBox.add("horse");
+				strBox.add("cow");
+				strBox.add("pig");
+				
+		System.out.println(strBox.get());
+		
 	}
 
 }
